7cfe439965ace05871007252b42c741a365341e3
smtsjhr <44889551+smtsjhr@users.noreply.github.com>
2023-05-29 16:03:16 -0700

    Update is_unextendible_product_basis.py
    use set() difference instead of list comprehension

--- toqito/state_props/is_unextendible_product_basis.py
+++ toqito/state_props/is_unextendible_product_basis.py
@@ -1,14 +1,13 @@
 """Check if a collection of vectors is an Unextendable Product Basis (UBP)."""
 
 from itertools import combinations
-import numpy as np
 
 def item_partitions(items, parts, sizes = None ):
     if sizes == None:
         sizes = [1]*parts
 
     if parts == 1:
-        return [items]
+        return [[items]]
     
     partitions = []
     min_choices = sizes[0]
@@ -16,7 +15,7 @@ def item_partitions(items, parts, sizes = None ):
     for j in range(min_choices, max_choices + 1):
         first_part = combinations(items, j)
         for part1 in first_part: 
-            unchosen_items = [item for item in items if item not in list(part1)]
+            unchosen_items = list(set(items) - set(part1))
             other_parts = item_partitions(unchosen_items, parts - 1, sizes[1:])
             for part2 in other_parts:
                 partition = [list(part1)] + [part2]
