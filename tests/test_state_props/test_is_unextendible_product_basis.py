68a857d77efa0101a14c7f381531f4ca6dffd052
smtsjhr <44889551+smtsjhr@users.noreply.github.com>
2023-06-08 23:54:45 -0700

    Update test_is_unextendible_product_basis.py
    added tests for GenShifts UPB

--- tests/test_state_props/test_is_unextendible_product_basis.py
+++ tests/test_state_props/test_is_unextendible_product_basis.py
@@ -21,6 +21,28 @@ tiles_B[:, 2] = [0, 1/np.sqrt(2), -1/np.sqrt(2)]
 tiles_B[:, 3] = [1, 0, 0]
 tiles_B[:, 4] = [1/np.sqrt(3), 1/np.sqrt(3), 1/np.sqrt(3)]
 
+def GenShifts(parties):
+    k = int((parties + 1)/2)
+    num_states = 2*k
+    upb = [np.zeros([2, num_states]) for _ in range(parties)]
+    local_states = [[np.cos(angle), np.sin(angle)] for angle in [(i/k)*np.pi/2 for i in range(2*k - 1, k, -1)] ]
+    orth_local_states = [[np.cos(angle), np.sin(angle)] for angle in [(i/k)*np.pi/2 for i in range(1,k)] ]
+
+    state_list = []
+    state_list.append([0,1])
+    state_list.extend(local_states)
+    state_list.extend(orth_local_states)
+
+    for party in upb:
+        party[:,0] = [1, 0]
+
+    for state in range(1, num_states):
+        for i, party in enumerate(upb):
+            party[:,state] = state_list[i]
+        state_list = state_list[-1:] + state_list[:-1]
+
+    return upb
+
 
 def test_is_unextendible_product_basis_input_empty_list():
     with np.testing.assert_raises(ValueError):
@@ -48,7 +70,7 @@ def test_is_unextendible_product_basis_input_arrays_not_same_num_columns():
 def test_is_unextendable_product_basis_tiles():
     res = is_unextendible_product_basis([tiles_A, tiles_B])
     expected_res = [True, None]
-    np.testing.assert_array_equal([res[0], res[1]], expected_res)
+    np.testing.assert_array_equal(res, expected_res)
 
 @pytest.mark.parametrize("num_states", [1, 2, 3, 4])
 def test_is_unextendable_product_basis_tiles_remove_states_false(num_states):
@@ -67,6 +89,12 @@ def test_is_unextendable_product_basis_tiles_remove_states_orthogonal_witness(nu
         ip_list.append(ip)
     expected_res = [0]*num_states
     np.testing.assert_array_almost_equal(ip, expected_res)
+
+@pytest.mark.parametrize("num_parties", [3, 5, 7])
+def test_is_unextendable_product_basis_tiles_GenShifts(num_parties):
+    res = is_unextendible_product_basis(GenShifts(num_parties))
+    expected_res = [True, None]
+    np.testing.assert_array_equal(res, expected_res)
 
 
 if __name__ == "__main__":
